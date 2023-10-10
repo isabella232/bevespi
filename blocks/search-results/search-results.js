4cbe4cd88bd3b105cd7294a8593f7de360db781e
Carlo Jelmini <jelmini@adobe.com>
2023-10-10 15:45:06 +0200

    chore: extracted search logic into a class

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -3,141 +3,152 @@ async function fetchData(path) {
   return response.json();
 }
 
-function updateURL(query, page = 1, pageSize = 10, curLocation = window.location) {
-  // eslint-disable-next-line no-restricted-globals
-  const { state } = history;
-  const { title } = document;
-  const url = new URL(curLocation.origin + curLocation.pathname);
-  url.searchParams.set('q', query);
-  url.searchParams.set('p', page.toString());
-  url.searchParams.set('s', pageSize.toString());
-  // eslint-disable-next-line no-restricted-globals
-  history.pushState(state, title, url.toString());
-}
+class SearchResults {
+  constructor(indexJson, container, paginationArrows, curLocation, pageSize) {
+    this.indexJson = indexJson;
+    this.container = container;
+    this.paginationArrows = paginationArrows;
+    this.curLocation = curLocation;
+    this.pageSize = pageSize;
+  }
 
-function search(indexJson, query, page, container, paginationArrows, curLocation, pageSize) {
-  updateURL(query.trim(), page, pageSize, curLocation);
-  // eslint-disable-next-line no-use-before-define
-  searchIndex(indexJson, query.trim(), page, container, paginationArrows, curLocation, pageSize);
-}
+  updateURL(query, page = 1) {
+    // eslint-disable-next-line no-restricted-globals
+    const { state } = history;
+    const { title } = document;
+    const url = new URL(this.curLocation.origin + this.curLocation.pathname);
+    url.searchParams.set('q', query.trim());
+    url.searchParams.set('p', page.toString());
+    url.searchParams.set('s', this.pageSize.toString());
+    // eslint-disable-next-line no-restricted-globals
+    history.pushState(state, title, url.toString());
+  }
 
-function searchIndex(indexJson, query, page, container, paginationArrows, curLocation, pageSize) {
-  container.querySelector('.results-container')?.remove();
-  container.querySelector('.title-container')?.remove();
-  container.querySelector('.paginationitems')?.remove();
+  search(query, page) {
+    this.updateURL(query.trim(), page);
+    this.searchIndex(query.trim(), page);
+  }
 
-  const titleContainer = document.createElement('div');
-  container.append(titleContainer);
-  titleContainer.classList.add('title-container');
-
-  function buildUserFeedbackContainer(...content) {
+  static #buildUserFeedbackContainer(titleContainer, ...content) {
     titleContainer.classList.add('user-feedback-message');
     const h2 = document.createElement('h2');
     titleContainer.append(h2);
     h2.append(...content);
     return h2;
   }
-  if (query) {
-    const showingSerResult = document.createElement('span');
-    showingSerResult.classList.add('showingSerResult');
-    showingSerResult.textContent = 'Showing results for: ';
-    titleContainer.append(showingSerResult);
-    const searchQuery = document.createElement('span');
-    searchQuery.classList.add('search-query');
-    searchQuery.textContent = query;
-    titleContainer.append(searchQuery);
-  } else {
-    buildUserFeedbackContainer(
-      'You did not enter any search terms.',
-      document.createElement('br'),
-      'Please enter a search term and try again.',
-    );
-    return;
-  }
 
-  const resultsContainer = document.createElement('div');
-  resultsContainer.classList.add('results-container');
-  container.append(resultsContainer);
-
-  let count = 0;
-
-  function highlightTerms(text, terms) {
+  static #highlightTerms(text, terms) {
     const pattern = terms.map((term) => `\\w*${term}\\w*`).join('|');
     return text.replace(new RegExp(pattern, 'gi'), (match) => `<strong>${match}</strong>`);
   }
 
-  indexJson.data.forEach((row) => {
-    const terms = query.trim().split(' ').filter((term) => term.length > 0);
-    if (terms.length > 0
-      && terms.every((term) => row.title.toLowerCase().includes(term.toLowerCase()))) {
-      count += 1;
-      if (count > (page - 1) * pageSize && count <= page * pageSize) {
-        const resultSection = document.createElement('div');
-        resultSection.classList.add('result');
-        const link = document.createElement('a');
-        link.href = row.path;
-        link.innerHTML = highlightTerms(row.title, terms);
-        resultSection.append(link);
-        resultsContainer.append(resultSection);
-      }
-    }
-  });
-  const pages = Math.ceil(count / pageSize);
-  if (count === 0) {
-    buildUserFeedbackContainer('We\'re sorry, but no results matched your search term.');
-  } else if (pages > 1) {
-    const paginationItems = document.createElement('ul');
-    paginationItems.classList.add('paginationitems');
-    container.append(paginationItems);
-    const previousPageArrow = document.createElement('li');
-    previousPageArrow.classList.add('nav-arrow', 'first');
-    if (page === 1) {
-      previousPageArrow.classList.add('nav-disabled');
-    } else {
-      const arrowImage = document.createElement('img');
-      [arrowImage.src] = paginationArrows;
-      previousPageArrow.append(arrowImage);
-      previousPageArrow.addEventListener('click', () => {
-        const activePage = Number(document.querySelector('li.active-page').textContent);
-        const prevPage = activePage - 1;
-        search(indexJson, query, prevPage, container, paginationArrows, curLocation, pageSize);
-      });
-    }
-    paginationItems.append(previousPageArrow);
+  searchIndex(query, page) {
+    this.container.querySelector('.results-container')?.remove();
+    this.container.querySelector('.title-container')?.remove();
+    this.container.querySelector('.paginationitems')?.remove();
 
-    let currentPage = 1;
-    while (currentPage <= pages) {
-      const li = document.createElement('li');
-      const span = document.createElement('span');
-      span.textContent = currentPage.toString();
-      if (currentPage === page) {
-        li.classList.add('active-page');
+    const titleContainer = document.createElement('div');
+    this.container.append(titleContainer);
+    titleContainer.classList.add('title-container');
+
+    if (query) {
+      const showingSerResult = document.createElement('span');
+      showingSerResult.classList.add('showingSerResult');
+      showingSerResult.textContent = 'Showing results for: ';
+      titleContainer.append(showingSerResult);
+      const searchQuery = document.createElement('span');
+      searchQuery.classList.add('search-query');
+      searchQuery.textContent = query;
+      titleContainer.append(searchQuery);
+    } else {
+      SearchResults.#buildUserFeedbackContainer(
+        titleContainer,
+        'You did not enter any search terms.',
+        document.createElement('br'),
+        'Please enter a search term and try again.',
+      );
+      return;
+    }
+
+    const resultsContainer = document.createElement('div');
+    resultsContainer.classList.add('results-container');
+    this.container.append(resultsContainer);
+
+    let count = 0;
+
+    this.indexJson.data.forEach((row) => {
+      const terms = query.trim().split(' ').filter((term) => term.length > 0);
+      if (terms.length > 0
+        && terms.every((term) => row.title.toLowerCase().includes(term.toLowerCase()))) {
+        count += 1;
+        if (count > (page - 1) * this.pageSize && count <= page * this.pageSize) {
+          const resultSection = document.createElement('div');
+          resultSection.classList.add('result');
+          const link = document.createElement('a');
+          link.href = row.path;
+          link.innerHTML = SearchResults.#highlightTerms(row.title, terms);
+          resultSection.append(link);
+          resultsContainer.append(resultSection);
+        }
+      }
+    });
+    const pages = Math.ceil(count / this.pageSize);
+    if (count === 0) {
+      SearchResults.#buildUserFeedbackContainer(titleContainer, 'We\'re sorry, but no results matched your search term.');
+    } else if (pages > 1) {
+      const paginationItems = document.createElement('ul');
+      paginationItems.classList.add('paginationitems');
+      this.container.append(paginationItems);
+      const previousPageArrow = document.createElement('li');
+      previousPageArrow.classList.add('nav-arrow', 'first');
+      if (page === 1) {
+        previousPageArrow.classList.add('nav-disabled');
       } else {
-        const pageNumber = currentPage;
-        span.addEventListener('click', () => {
-          search(indexJson, query, pageNumber, container, paginationArrows, curLocation, pageSize);
+        const arrowImage = document.createElement('img');
+        [arrowImage.src] = this.paginationArrows;
+        previousPageArrow.append(arrowImage);
+        previousPageArrow.addEventListener('click', () => {
+          const activePage = Number(document.querySelector('li.active-page').textContent);
+          const prevPage = activePage - 1;
+          this.search(query, prevPage);
         });
       }
-      li.append(span);
-      paginationItems.append(li);
-      currentPage += 1;
-    }
+      paginationItems.append(previousPageArrow);
 
-    const nextPageArrow = document.createElement('li');
-    nextPageArrow.classList.add('nav-arrow', 'last');
-    if (page === pages) {
-      nextPageArrow.classList.add('nav-disabled');
-    } else {
-      const arrowImage = document.createElement('img');
-      [, arrowImage.src] = paginationArrows;
-      nextPageArrow.append(arrowImage);
-      nextPageArrow.addEventListener('click', () => {
-        const activePage = Number(document.querySelector('li.active-page').textContent);
-        const nextPage = activePage + 1;
-        search(indexJson, query, nextPage, container, paginationArrows, curLocation, pageSize);
-      });
+      let currentPage = 1;
+      while (currentPage <= pages) {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.textContent = currentPage.toString();
+        if (currentPage === page) {
+          li.classList.add('active-page');
+        } else {
+          const pageNumber = currentPage;
+          span.addEventListener('click', () => {
+            this.search(query, pageNumber);
+          });
+        }
+        li.append(span);
+        paginationItems.append(li);
+        currentPage += 1;
+      }
+
+      const nextPageArrow = document.createElement('li');
+      nextPageArrow.classList.add('nav-arrow', 'last');
+      if (page === pages) {
+        nextPageArrow.classList.add('nav-disabled');
+      } else {
+        const arrowImage = document.createElement('img');
+        [, arrowImage.src] = this.paginationArrows;
+        nextPageArrow.append(arrowImage);
+        nextPageArrow.addEventListener('click', () => {
+          const activePage = Number(document.querySelector('li.active-page').textContent);
+          const nextPage = activePage + 1;
+          this.search(query, nextPage);
+        });
+      }
+      paginationItems.append(nextPageArrow);
     }
-    paginationItems.append(nextPageArrow);
   }
 }
 
@@ -204,39 +215,22 @@ export default async function decorate(block, curLocation = window.location) {
   const searchResultsContainer = document.createElement('div');
   searchResultsContainer.classList.add('search-results', 'section');
   searchSection.append(searchResultsContainer);
-
-  searchButton.addEventListener('click', () => {
-    search(
-      indexJson,
-      searchInput.value,
-      page,
-      searchResultsContainer,
-      paginationArrowsSrc,
-      curLocation,
-      pageSize,
-    );
-  });
-  searchInput.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-      search(
-        indexJson,
-        searchInput.value,
-        page,
-        searchResultsContainer,
-        paginationArrowsSrc,
-        curLocation,
-        pageSize,
-      );
-    }
-  });
-
-  searchIndex(
+  const searchResults = new SearchResults(
     indexJson,
-    query,
-    page,
     searchResultsContainer,
     paginationArrowsSrc,
     curLocation,
     pageSize,
   );
+
+  searchButton.addEventListener('click', () => {
+    searchResults.search(searchInput.value, page);
+  });
+  searchInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      searchResults.search(searchInput.value, page);
+    }
+  });
+
+  searchResults.searchIndex(query, page);
 }
