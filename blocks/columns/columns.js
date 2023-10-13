1fb641b6f660f18722bfa5cc310dd57ec039945d
Mircea Salan <salan.mircea@gmail.com>
2023-10-13 18:18:52 +0300

    Fix home style issues

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,9 +1,30 @@
+
+const addRowWrapper = function(row) {
+  const rowWrapper = document.createElement('div');
+  rowWrapper.classList.add('row-wrapper');
+  const rowContainer = row;
+  row.replaceWith(rowWrapper);
+  rowWrapper.appendChild(rowContainer);
+}
+
+const setContainerBackgroundImage = function(picture, col) {
+  const img = picture.querySelector('img');
+  if(img) {
+    const rowWrapper = col.closest('.row-wrapper');
+    if (rowWrapper) {
+      rowWrapper.style = `background: url(${img.src}); background-size: cover;`;
+      picture.remove();
+    }
+  }
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
   [...block.children].forEach((row) => {
+    addRowWrapper(row);
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
@@ -11,6 +32,8 @@ export default function decorate(block) {
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
+        } else if(pic.parentElement.tagName === 'P') {
+          setContainerBackgroundImage(pic, col);
         }
       }
     });
