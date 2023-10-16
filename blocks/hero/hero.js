f4ebd8f318d2b4b496ba9d662db3a16868a8e121
Alberto Di Cagno <adicagno@adobe.com>
2023-10-16 16:57:11 +0200

    feat: home hero scroll

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,1 +1,14 @@
-// nothing to do yet
+/**
+ * loads and decorates the footer
+ * @param {Element} block The hero block element
+ */
+export default async function decorate(block) {
+  if (block.classList.contains('home')) {
+    const btnScroll = document.createRange()
+      .createContextualFragment(`<div class="btn-scroll">
+<a href="#how-bevespiaerosphere-can-help">Scroll
+<img src="/icons/icon_pink-arrow-down.png" alt="Scroll to section 2 header"/></a>
+</div>`);
+    block.appendChild(btnScroll);
+  }
+}
