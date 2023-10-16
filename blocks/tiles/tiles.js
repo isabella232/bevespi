9de8619c600a7f9b35eeab2d2b993cce68d2e416
Hannes Hertach <hanneshertach@gmail.com>
2023-10-16 17:36:01 +0200

    create grid for tiles

---
+++ blocks/tiles/tiles.js
@@ -0,0 +1,23 @@
+export default function decorate(block) {
+  block.querySelectorAll(':scope > div').forEach((tile) => {
+    const cover = tile.querySelector(':scope > div:first-child');
+    const modalContent = tile.querySelector(':scope > div:nth-child(2)');
+    const properties = tile.querySelector(':scope > div:nth-child(3)');
+
+    const picture = cover.querySelector('picture');
+    picture.closest('p').replaceWith(picture);
+
+    if (modalContent.classList.contains('button-container')) {
+      // only has a button. Then the tile becomes a link
+      const a = modalContent.querySelector('a');
+      a.innerHTML = '';
+      a.append(cover);
+      a.parentElement.replaceWith(a);
+      a.classList.add('cover');
+      a.classList.remove('button');
+    } else {
+      modalContent.classList.add('modal');
+      cover.classList.add('cover', 'with-modal');
+    }
+  });
+}
