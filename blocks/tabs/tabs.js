e27b80eb0240f5694f44c7baec83aeafa34cf7aa
Hannes Hertach <hanneshertach@gmail.com>
2023-10-16 14:10:53 +0200

    load fragments in tab block

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,3 +1,6 @@
+import { decorateMain } from '../../scripts/scripts.js';
+import { loadBlocks } from '../../scripts/aem.js';
+
 function deselectAllPanels(block) {
   block.querySelectorAll('button[role="tab"]').forEach((button) => {
     button.setAttribute('aria-selected', 'false');
@@ -7,11 +10,32 @@ function deselectAllPanels(block) {
   });
 }
 
-export default function decorate(block) {
+async function loadFragment(url) {
+  let path = url;
+  if (url && !url.startsWith('/')) {
+    path = new URL(url).pathname;
+  }
+
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(`${path}.plain.html`);
+    if (resp.ok) {
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+      decorateMain(main);
+      await loadBlocks(main);
+      return main;
+    }
+  }
+  return null;
+}
+
+export default async function decorate(block) {
   const tabNames = [...block.querySelectorAll(':scope > div > div:first-child')].map((div) => div.textContent);
   const tabLinks = [...block.querySelectorAll(':scope > div > div:nth-child(2)')].map((div) => div.textContent);
 
   block.innerHTML = '';
+
+  const fragments = await Promise.all(tabLinks.map((link) => loadFragment(link)));
 
   block.append(document.createRange().createContextualFragment(`
     <div class='tab-controls-wrapper'>
@@ -25,7 +49,7 @@ export default function decorate(block) {
     </div>
     <div class="tab-outlets">
       ${tabLinks.map((tabLink, i) => `
-        <div id="panel-${i}" role="tabpanel" aria-selected="${i === 0 ? 'true' : 'false'}">${tabLink}</div>
+        <div id="panel-${i}" role="tabpanel" aria-selected="${i === 0 ? 'true' : 'false'}"></div>
       `).join('')}
     </div>
   `));
@@ -36,5 +60,10 @@ export default function decorate(block) {
       button.setAttribute('aria-selected', 'true');
       block.querySelector(`div[role="tabpanel"][id="panel-${i}"]`).setAttribute('aria-selected', 'true');
     });
+  });
+
+  document.querySelectorAll('div[role="tabpanel"]').forEach((panel, i) => {
+    console.log(fragments);
+    panel.append(fragments[i]);
   });
 }
