2cb748672d7ad0913b1b4d2b8c81fd095da4f526
Hannes Hertach <hanneshertach@gmail.com>
2023-10-17 11:37:08 +0200

    style tiles

--- blocks/tiles/tiles.js
+++ blocks/tiles/tiles.js
@@ -1,5 +1,7 @@
 export default function decorate(block) {
   block.querySelectorAll(':scope > div').forEach((tile) => {
+    tile.classList.add('tile');
+
     const cover = tile.querySelector(':scope > div:first-child');
     const modalContent = tile.querySelector(':scope > div:nth-child(2)');
     const properties = tile.querySelector(':scope > div:nth-child(3)');
@@ -15,13 +17,29 @@ export default function decorate(block) {
       a.parentElement.replaceWith(a);
       a.classList.add('cover');
       a.classList.remove('button');
+
+      tile.append(document.createRange().createContextualFragment(`
+        <img class="bottom-icon" src="/icons/icon-redirect.png" />
+      `));
     } else {
+      // there is a modal. The tile opens a modal.
       modalContent.classList.add('modal');
       cover.classList.add('cover', 'with-modal');
-    }
 
-    cover.addEventListener('click', (e) => {
-      cover.toggleAttribute('aria-expanded');
-    });
+      modalContent.append(document.createRange().createContextualFragment(`
+        <button name="close-modal"><img src="/icons/modal-close.png" alt="close"/></button>
+      `));
+
+      cover.addEventListener('click', () => {
+        cover.toggleAttribute('aria-expanded');
+      });
+      modalContent.querySelector('button[name="close-modal"]').addEventListener('click', () => {
+        cover.toggleAttribute('aria-expanded');
+      });
+
+      tile.append(document.createRange().createContextualFragment(`
+        <img class="bottom-icon" src="/icons/icon-expand.png" />
+      `));
+    }
   });
 }
