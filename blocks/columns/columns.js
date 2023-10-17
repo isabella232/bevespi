ec903e4331dedbba15612038e91451f18cfa4149
Mircea Salan <salan.mircea@gmail.com>
2023-10-17 18:48:38 +0300

    revert js changes on column

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,21 +1,3 @@
-function addRowWrapper(row) {
-  const rowWrapper = document.createElement('div');
-  rowWrapper.classList.add('row-wrapper');
-  [...row.children].forEach((rowChildren) => {
-    rowWrapper.appendChild(rowChildren);
-  });
-  row.appendChild(rowWrapper);
-}
-
-function markFootnotes(col) {
-  const italicNotes = col.querySelectorAll('em');
-  [...italicNotes].forEach((note) => {
-    if (note.textContent[0] === '*') {
-      note.classList.add('footnote');
-    }
-  });
-}
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -23,9 +5,7 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     row.classList.add('columns-row');
-    addRowWrapper(row);
     [...row.children].forEach((col) => {
-      markFootnotes(col);
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
