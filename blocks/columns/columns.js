f804a2c583d6e931e1ab75034620bc2fc46b2536
Paolo Mottadelli <paolo.moz@gmail.com>
2023-10-18 11:18:45 +0200

    page: copd-treatment

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -5,7 +5,9 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     row.classList.add('columns-row');
+    const rowInner = document.createElement('div');
     [...row.children].forEach((col) => {
+      col.classList.add('columns-col');
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
@@ -14,6 +16,8 @@ export default function decorate(block) {
           picWrapper.classList.add('columns-img-col');
         }
       }
+      rowInner.appendChild(col);
     });
+    row.appendChild(rowInner);
   });
 }
