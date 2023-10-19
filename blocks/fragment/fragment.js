e984c1fc37f53a2401129ab7bbbb24db6d737fe6
Mircea Salan <salan.mircea@gmail.com>
2023-10-19 11:13:35 +0300

    Remove fragment changes

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -87,9 +87,7 @@ function renderStickyFragmentInformationBlock(block) {
   if (informationBlockHeader) {
     const copyOfInfoBlockHeader = informationBlockHeader.cloneNode(informationBlockHeader);
     stickyInformationBlockHeaderContainer.appendChild(copyOfInfoBlockHeader);
-    const informationBlockScrollButton = document.createElement('div');
-    informationBlockScrollButton.role = 'button';
-    informationBlockScrollButton.ariaLabel = 'Scroll to Important Safety Information';
+    const informationBlockScrollButton = document.createElement('a');
     informationBlockScrollButton.classList.add('sticky-fragment-scroll-button');
     informationBlockScrollButton.addEventListener(
       'click',
