2e504f09a48c866ff35d19920e734c1f75b4e591
Paolo Mottadelli <paolo.moz@gmail.com>
2023-10-20 10:09:23 +0200

    fix: eslint

--- blocks/next/next.js
+++ blocks/next/next.js
@@ -1,10 +1,11 @@
 export default async function decorate(block) {
   const link = block.querySelector('a');
-  link.innerHTML = `<span class="next-large">NEXT:</span>
+  link.innerHTML = `
+    <span class="next-large">NEXT:</span>
     <span class="next-small">${link.textContent}</span>
     <span class="next-icon"></span>`;
 
-  let observer = new IntersectionObserver(function (entries, observer) {
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => { if (entry.isIntersecting) entry.target.classList.add('active'); });
   });
   observer.observe(block);
