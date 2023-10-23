fa382e4db324f780241ae0869a241b0b26faf6a2
Paolo Mottadelli <paolo.moz@gmail.com>
2023-10-23 12:13:47 +0200

    accordion 1

---
+++ blocks/accordion-1/accordion-1.js
@@ -0,0 +1,11 @@
+export default async function decorate(block) {
+  [...block.children].forEach((row) => {
+    const button = document.createElement('span');
+    row.prepend(button);
+    button.addEventListener('click', (e) => { e.target.parentElement.parentElement.classList.toggle('active'); });
+
+    const rowInner = document.createElement('div');
+    [...row.children].forEach((child) => { rowInner.appendChild(child); });
+    row.appendChild(rowInner);
+  })
+}
