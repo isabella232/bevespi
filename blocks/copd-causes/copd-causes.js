f150965eae2ae1a56693cfa7ca4a6c6193e357fd
Alberto Di Cagno <adicagno@adobe.com>
2023-10-25 12:10:34 +0200

    fix: dynamic layout switch

--- blocks/copd-causes/copd-causes.js
+++ blocks/copd-causes/copd-causes.js
@@ -11,20 +11,22 @@ function onResize(block) {
     const isDesktop = x.matches;
     if (isDesktop) {
       block.innerHTML = originalBlock.innerHTML;
-      const ems = block.querySelectorAll('div > div:nth-child(2) em');
-      // eslint-disable-next-line no-restricted-syntax
-      for (const em of ems) {
-        const dstDiv = block.querySelector('div > div:nth-child(3)');
-        const clonedEm = em.cloneNode(true);
-        dstDiv.prepend(clonedEm);
-        em.remove();
-      }
+      // this section depends on the content and is enabled for the first
+      // row, second col in the word doc table
+      // TODO: define a slightly different layout to be consistent across
+      // the different viewport widths
+      const em = block.querySelector('div > div:nth-child(2) em:first-of-type');
+      const dstDiv = block.querySelector('div > div:nth-child(3)');
+      const clonedEm = em.cloneNode(true);
+      dstDiv.prepend(clonedEm);
+      em.remove();
     } else {
       // reset to the original contents
       block.innerHTML = originalBlock.innerHTML;
     }
   };
 }
+
 /**
  * loads and decorates the COPD Causes block
  * @param {Element} block The COPD Causes block element
