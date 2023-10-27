0916dbdf966ae929cec4f08a15d05da41b4dc164
Paolo Mottadelli <paolo.moz@gmail.com>
2023-10-27 11:37:54 +0200

    accordion 2 fixes

--- blocks/accordion-2/accordion-2.js
+++ blocks/accordion-2/accordion-2.js
@@ -3,7 +3,15 @@ export default async function decorate(block) {
     if (row.querySelector('picture')) row.querySelector('picture').parentElement.classList.add('p-img-first');
     else row.querySelector('div').innerHTML = `<p class='p-img-first'></p>${row.querySelector('div').innerHTML}`;
     row.querySelector('h2 + p')?.classList.add('p-first');
-    [...row.querySelectorAll('picture')].forEach((picture) => { picture.parentElement.classList.add('p-img'); });
+    [...row.querySelectorAll('p:nth-child(n+2) picture')].forEach((picture) => {
+      const pImg = picture.parentElement;
+      pImg.classList.add('p-img');
+      const span = document.createElement('span');
+      span.innerHTML = pImg.innerHTML;
+      pImg.innerHTML = "";
+      pImg.prepend(span.querySelector('picture'));
+      pImg.append(span);
+    });
 
     const buttons = document.createElement('div');
     buttons.classList.add('buttons');
