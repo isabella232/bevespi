a0a5e4b30eb7166d40c0034663d7b01b1108d81b
Paolo Mottadelli <paolo.moz@gmail.com>
2023-11-01 09:31:10 +0100

    scroll page navigation current link

--- blocks/page-navigation/page-navigation.js
+++ blocks/page-navigation/page-navigation.js
@@ -8,7 +8,16 @@ export default async function decorate(block) {
     const nav = document.createElement('nav');
     nav.innerHTML = html;
     [...nav.querySelectorAll('a')].forEach((link) => {
-      if (new URL(link).pathname === window.location.pathname) link.classList.add('current');
+      if (new URL(link).pathname === window.location.pathname) {
+        link.classList.add('current');
+        var observer = new IntersectionObserver((entries, observer) => {
+          entries.forEach(entry => {
+            const div = link.closest('ul').parentElement;
+            div.scrollTo({ left: (link.offsetLeft - div.offsetLeft - 15), behavior: 'smooth' });
+          });
+        });
+        observer.observe(link);
+      }
     });
     block.innerHTML = '';
     block.append(nav);
