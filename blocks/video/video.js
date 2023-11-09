64d6b75f00f5508c1d8b65ad0a6165161e06ccfa
Mircea Salan <salan.mircea@gmail.com>
2023-11-09 15:48:21 +0200

    make video contained

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,20 +1,21 @@
 import { loadScript } from '../../scripts/scripts.js';
 
+
 export default function decorate(block) {
   const playerID = block.querySelector('div:nth-of-type(1) > div:nth-of-type(2)')?.textContent;
   const entryID = block.querySelector('div:nth-of-type(2) > div:nth-of-type(2)')?.textContent;
   [...block.querySelectorAll('div:nth-of-type(-n+2)')].forEach((div) => div.remove());
 
   const tagDiv = document.createElement('div');
-  tagDiv.id = `kaltura_player_${playerID}`;
-  tagDiv.style.width = '645px';
-  tagDiv.style.height = '370px';
+  tagDiv.classList.add('video-player');
+  const tagDivId = `kaltura_player_${playerID}`;
+  tagDiv.id = tagDivId;
   block.append(tagDiv);
 
   loadScript('https://cdnapisec.kaltura.com/p/432521/sp/43252100/embedIframeJs/uiconf_id/52784152/partner_id/432521', () => {
     // eslint-disable-next-line
-    kWidget.embed({
-      targetId: `kaltura_player_${playerID}`,
+    window.kWidget.embed({
+      targetId: tagDivId,
       wid: '_432521',
       uiconf_id: 52784152,
       flashvars: {},
@@ -22,4 +23,6 @@ export default function decorate(block) {
       entry_id: entryID,
     });
   });
+
+  return;
 }
