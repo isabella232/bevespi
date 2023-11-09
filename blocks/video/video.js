22b2674a506ee941c50892e1355f3482a9df1a98
Mircea Salan <salan.mircea@gmail.com>
2023-11-09 19:07:28 +0200

    video enhancement

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,28 +1,19 @@
 import { loadScript } from '../../scripts/scripts.js';
 
-let playerID = '';
-let entryID = '';
+export default function decorate(block) {
+  const playerID = block.querySelector('div:nth-of-type(1) > div:nth-of-type(2)')?.textContent;
+  const entryID = block.querySelector('div:nth-of-type(2) > div:nth-of-type(2)')?.textContent;
+  [...block.querySelectorAll('div:nth-of-type(-n+2)')].forEach((div) => div.remove());
 
-function getTagDivId() {
-  return `kaltura_player_${playerID}`;
-}
-
-function removeVideoPlaceholder(block) {
-  const videoPlaceholder = block.querySelector(`#${getTagDivId()}`);
-  if (videoPlaceholder) {
-    videoPlaceholder.remove();
-  }
-}
-
-function loadVideo(block) {
   const tagDiv = document.createElement('div');
   tagDiv.classList.add('video-player');
   const tagDivId = `kaltura_player_${playerID}`;
   tagDiv.id = tagDivId;
   block.append(tagDiv);
 
-  loadScript('https://cdnapisec.kaltura.com/p/432521/sp/43252100/embedIframeJs/uiconf_id/52784152/partner_id/432521', () => {
-    removeVideoPlaceholder(block);
+  window.setTimeout(() => {
+    loadScript('https://cdnapisec.kaltura.com/p/432521/sp/43252100/embedIframeJs/uiconf_id/52784152/partner_id/432521', () => {
+
     // eslint-disable-next-line
     kWidget.embed({
       targetId: tagDivId,
@@ -35,24 +26,5 @@ function loadVideo(block) {
       entry_id: entryID,
     });
   });
-}
-
-function createVideoPlaceholder(block) {
-  const videoPlaceholderWrapper = document.createElement('div');
-  videoPlaceholderWrapper.classList.add('video-placeholder-wrapper');
-  videoPlaceholderWrapper.id = getTagDivId();
-  const videoPlaceholder = document.createElement('div');
-  videoPlaceholder.classList.add('video-placeholder');
-  videoPlaceholder.addEventListener('click', loadVideo.bind(null, block));
-  videoPlaceholderWrapper.append(videoPlaceholder);
-
-  block.append(videoPlaceholderWrapper);
-}
-
-export default function decorate(block) {
-  playerID = block.querySelector('div:nth-of-type(1) > div:nth-of-type(2)')?.textContent;
-  entryID = block.querySelector('div:nth-of-type(2) > div:nth-of-type(2)')?.textContent;
-  [...block.querySelectorAll('div:nth-of-type(-n+2)')].forEach((div) => div.remove());
-
-  createVideoPlaceholder(block);
+  }, 3000);
 }
